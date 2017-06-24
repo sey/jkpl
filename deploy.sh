e38b3141c8528ba7db4d1af75e3dee97715dcdaa
Florian Sey <florian.sey@gmail.com>
2017-06-24 16:25:34 +0200

    Update.

--- deploy.sh
+++ deploy.sh
@@ -11,30 +11,30 @@ TMP_FILE=${TMP_DIR}/${PACKAGE_FILE}
 TARGET_DIR=/var/www/${PACKAGE_NAME}
 
 if [ -d ${TMP_DIR} ]; then
-  rm -rf ${TMP_DIR}
+  rm -rf ${TMP_DIR} || exit
 fi
 
 echo 'Create tmp directory:' ${TMP_DIR}
-mkdir -p ${TMP_DIR}
+mkdir -p ${TMP_DIR} || exit
 
 echo 'Download file: wget -O' ${TMP_FILE} ${PACKAGE_URL}
-wget --user=${NPM_REPO_USR} --password=${NPM_REPO_USR} -O ${TMP_FILE} ${PACKAGE_URL}
+wget --user=${NPM_REPO_USR} --password=${NPM_REPO_USR} -O ${TMP_FILE} ${PACKAGE_URL} || exit
 
 echo 'Extract file:' tar xf ${TMP_FILE} '-C' ${TMP_DIR}
-tar xf ${TMP_FILE} -C ${TMP_DIR}
+tar xf ${TMP_FILE} -C ${TMP_DIR} || exit
 
-pushd ${TMP_DIR}/package 
+pushd ${TMP_DIR}/package || exit
 
-npm install --production
+npm install --production || exit
 
-popd
+popd || exit
 
 if [ -d ${TARGET_DIR} ]; then
-  sudo rm -rf ${TARGET_DIR}/*
+  sudo rm -rf ${TARGET_DIR}/* || exit
 fi
 
-sudo mkdir -p ${TARGET_DIR}
+sudo mkdir -p ${TARGET_DIR} || exit
 
-sudo mv ${TMP_DIR}/package/* ${TARGET_DIR}
+sudo mv ${TMP_DIR}/package/* ${TARGET_DIR} || exit
 
-pm2 start ${TARGET_DIR}/src/app.js --name jkpl
+pm2 start ${TARGET_DIR}/src/app.js --name jkpl || exit
