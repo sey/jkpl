e44b22772340e1bbd7f34d7c8a96d6ddf9dbc056
Florian Sey <florian.sey@gmail.com>
2017-06-25 03:26:31 +0200

    Update.

--- src/app.js
+++ src/app.js
@@ -4,10 +4,7 @@ const app = express()
 
 app.get('/', (req, res) => {
   console.time(req.url)
-  res.send('Hello Florian!')
-  console.log('response sent')
-  console.warn('response sent')
-  console.error('response sent')
+  res.send('Hello Florian Sey!')
   console.timeEnd(req.url)
 })
 
